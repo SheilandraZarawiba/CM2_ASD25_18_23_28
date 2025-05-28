ec36858f54ae4ca700091925aa6cc17519b3015d
izza <muhammadibnzzi@gmail.com>
2025-05-28 08:41:47 +0700

    class pasien, node dan daftar pasien

---
+++ src/Pasien.java
@@ -0,0 +1,11 @@
+public class Pasien {
+    String nama;
+    String nik;
+    String keluhan;
+
+    public Pasien(String nama, String nik, String keluhan) {
+        this.nama = nama;
+        this.nik = nik;
+        this.keluhan = keluhan;
+    }
+}
