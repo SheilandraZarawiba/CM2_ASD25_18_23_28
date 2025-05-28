4e3d55488bebd5b7dae972b35cdfae042a6a618d
Rafi Abyantara Pratama <rafiabyantarapratama@gmail.com>
2025-05-28 09:59:36 +0700

    rafi_DataDokter

--- src/KlinikDokterMain.java
+++ src/KlinikDokterMain.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 
 public class KlinikDokterMain {
     public static void main(String[] args) {
-        Scanner sc =  new Scanner(System.in);
+         Scanner sc =  new Scanner(System.in);
         int pilihan;
 
         
